88d1fbfb9f6cd3d3d6d4774136f2224d9d12cf91
Samuel Michaux <kameleon836@gmail.com>
2020-08-21 16:57:18 +0200

    select ok, insert ok, delete ok

--- src/main/java/com/formation/startJDBC/StartJdbc.java
+++ src/main/java/com/formation/startJDBC/StartJdbc.java
@@ -2,9 +2,7 @@ package com.formation.startJDBC;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class StartJdbc {
@@ -30,9 +28,53 @@ public class StartJdbc {
         try {
             Connection connection =  DriverManager.getConnection(url, login, password);
 
+            // 978-2-07-508252-5
+            String query = "select * from livre where isbn = ?";
+            try (PreparedStatement st = connection.prepareStatement(query)){
+                st.setString(1, "978-2-07-508255-6");
+
+                ResultSet res = st.executeQuery();
+
+                while(res.next()){
+//                    System.out.println(res.getString(2));
+
+                    System.out.printf("l'isbn: %s, titre: %s, nom de l'auteur: %s, prénom de l'auteur: %s, l'éditeur: %s, l'année: %d\n",
+                            res.getString("isbn"),
+                            res.getString("titre"),
+                            res.getString("auteur_nom"),
+                            res.getString("auteur_prenom"),
+                            res.getString("editeur"),
+                            res.getInt("annee")
+                            );
+                }
 
 
+            }catch (SQLException s){
+                s.printStackTrace();
+            }
 
+            query = "INSERT into livre values (?,?,?,?,?,?)";
+            try (PreparedStatement st = connection.prepareStatement(query) ){
+                st.setString(1, "978-2070368228");
+                st.setString(2, "1984");
+                st.setString(3, "george");
+                st.setString(4, "orwell");
+                st.setString(5, "Gallimard");
+                st.setInt(6, 1972);
+
+                int nb = st.executeUpdate();
+                System.out.printf("%d ligne(s) update\n", nb);
+            }
+
+            query = "DELETE from livre where isbn = ?";
+            try (PreparedStatement st = connection.prepareStatement(query) ){
+                st.setString(1, "978-2070368228");
+
+                int nb = st.executeUpdate();
+                System.out.printf("%d ligne(s) update\n", nb);
+            }
+
+            connection.close();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
