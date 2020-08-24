216875456b6ac6de94d5e4568991f8fbd35fd47e
Samuel Michaux <kameleon836@gmail.com>
2020-08-24 11:43:17 +0200

    Sonar ok without refractoring and System.out

--- src/main/java/com/formation/start_jdbc/StartJdbc.java
+++ src/main/java/com/formation/start_jdbc/StartJdbc.java
@@ -1,22 +1,16 @@
 package com.formation.start_jdbc;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
 public class StartJdbc {
-
-    private static final Logger log = LogManager.getLogger(StartJdbc.class.getSimpleName());
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Properties props = new Properties();
-        try (FileInputStream fis = new FileInputStream("conf.properties")) {
+        try (FileInputStream fis = new FileInputStream("conf.properties")){
             props.load(fis);
-        } catch (IOException ioException) {
+        }catch (IOException ioException){
             ioException.printStackTrace();
         }
 
@@ -31,35 +25,33 @@ public class StartJdbc {
             e.printStackTrace();
         }
 
-        try (Connection connection = DriverManager.getConnection(url, login, password)) {
-
+        try(Connection connection =  DriverManager.getConnection(url, login, password)) {
             String query = "select * from livre where isbn = ?";
-            try (PreparedStatement st = connection.prepareStatement(query)) {
+            try (PreparedStatement st = connection.prepareStatement(query)){
                 st.setString(1, "978-2-07-508255-6");
 
                 try (ResultSet res = st.executeQuery()) {
 
                     while (res.next()) {
-                        log.info("l'isbn: {}, titre: {}, nom de l'auteur: {}, prénom de l'auteur: {}, l'éditeur: {}, l'année: {}",
+
+                        System.out.printf("l'isbn: %s, titre: %s, nom de l'auteur: %s, prénom de l'auteur: %s, l'éditeur: %s, l'année: %d%n",
                                 res.getString("isbn"),
                                 res.getString("titre"),
                                 res.getString("auteur_nom"),
                                 res.getString("auteur_prenom"),
                                 res.getString("editeur"),
-                                res.getInt("annee"));
-
+                                res.getInt("annee")
+                        );
                     }
                 }
 
 
-            } catch (SQLException s) {
+            }catch (SQLException s){
                 s.printStackTrace();
             }
 
-            // -------------------------------
-
             query = "INSERT into livre values (?,?,?,?,?,?)";
-            try (PreparedStatement st = connection.prepareStatement(query)) {
+            try (PreparedStatement st = connection.prepareStatement(query) ){
                 st.setString(1, "978-2070368228");
                 st.setString(2, "1984");
                 st.setString(3, "george");
@@ -68,22 +60,17 @@ public class StartJdbc {
                 st.setInt(6, 1972);
 
                 int nb = st.executeUpdate();
-                log.error("{} ligne(s) update", nb);
-            } catch (SQLException s) {
-                s.printStackTrace();
+                System.out.printf("%d ligne(s) update%n", nb);
             }
 
-            // ------------------
-
             query = "DELETE from livre where isbn = ?";
-            try (PreparedStatement st = connection.prepareStatement(query)) {
+            try (PreparedStatement st = connection.prepareStatement(query) ){
                 st.setString(1, "978-2070368228");
 
                 int nb = st.executeUpdate();
-                log.error("{} ligne(s) update", nb);
-            } catch (SQLException s) {
-                s.printStackTrace();
+                System.out.printf("%d ligne(s) update%n", nb);
             }
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
